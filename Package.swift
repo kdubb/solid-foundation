ef5b733e9e1e3c0310f48e7b721b25b85e433c5e
Kevin Wooten <kevin@wooten.com>
2025-04-05 07:31:56 -0700

    Fix Swift package resource warnings

--- Package.swift
+++ Package.swift
@@ -18,32 +18,35 @@ let package = Package(
       targets: ["Codex"]),
   ],
   dependencies: [
-    .package(url: "https://github.com/apple/swift-collections.git", .upToNextMinor(from: "1.1.4")),
     .package(url: "https://github.com/apple/swift-atomics.git", from: "1.2.0"),
-    .package(url: "https://github.com/antlr/antlr4.git", from: "4.13.2"),
+    .package(url: "https://github.com/apple/swift-collections.git", .upToNextMinor(from: "1.1.4")),
     .package(url: "https://github.com/mgriebling/BigDecimal.git", from: "3.0.2"),
     .package(url: "https://github.com/SwiftScream/URITemplate.git", from: "5.0.1"),
+    .package(url: "https://github.com/antlr/antlr4.git", from: "4.13.2"),
   ],
   targets: [
     .target(
       name: "Codex",
       dependencies: [
+        .product(name: "Atomics", package: "swift-atomics"),
         .product(name: "Collections", package: "swift-collections"),
         .product(name: "BigDecimal", package: "BigDecimal"),
-        .product(name: "Antlr4", package: "Antlr4"),
-        .product(name: "Atomics", package: "swift-atomics"),
         .product(name: "ScreamURITemplate", package: "uritemplate"),
+        .product(name: "Antlr4", package: "Antlr4"),
       ],
       exclude: [
-        "*.interp",
-        "*.tokens",
-        "*.g4",
+        "Path/Parsing/Path.g4",
+        "Path/Parsing/Path.interp",
+        "Path/Parsing/Path.tokens",
+        "Path/Parsing/PathLexer.interp",
+        "Path/Parsing/PathLexer.tokens",
       ]
     ),
     .testTarget(
       name: "CodexTests",
       dependencies: ["Codex"],
-      exclude: ["JSONTestSuite"],
+      // Reference directory with no Swift/C files to remove SPM warnings
+      sources: ["JSONTestSuite/bin"],
       resources: [
         .copy("JSONTestSuite/tests"),
         .copy("JSONTestSuite/output-tests"),
